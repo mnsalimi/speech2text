3d700abe6c0953a86eef09cbe17baaa757a31fdb
mnsalimi <sartakhti.salimi@gmail.com>
2022-06-16 22:20:00 +0430

    training ngrams finished

--- ngrams.py
+++ ngrams.py
@@ -1,77 +1,50 @@
-# imports
-import string
-import random
-import nltk
-nltk.download('punkt')
-nltk.download('stopwords')
-nltk.download('reuters')
-from nltk.corpus import reuters
-from nltk import FreqDist
- 
-# input the reuters sentences
-sents  =reuters.sents()
- 
-# write the removal characters such as : Stopwords and punctuation
-stop_words = set(stopwords.words('english'))
-string.punctuation = string.punctuation +'"'+'"'+'-'+'''+'''+'—'
-string.punctuation
-removal_list = list(stop_words) + list(string.punctuation)+ ['lt','rt']
-removal_list
- 
-# generate unigrams bigrams trigrams
-unigram=[]
-bigram=[]
-trigram=[]
-tokenized_text=[]
-for sentence in sents:
-  sentence = list(map(lambda x:x.lower(),sentence))
-  for word in sentence:
-        if word== '.':
-            sentence.remove(word)
-        else:
-            unigram.append(word)
-   
-  tokenized_text.append(sentence)
-  bigram.extend(list(ngrams(sentence, 2,pad_left=True, pad_right=True)))
-  trigram.extend(list(ngrams(sentence, 3, pad_left=True, pad_right=True)))
- 
-# remove the n-grams with removable words
-def remove_stopwords(x):    
-    y = []
-    for pair in x:
-        count = 0
-        for word in pair:
-            if word in removal_list:
-                count = count or 0
-            else:
-                count = count or 1
-        if (count==1):
-            y.append(pair)
-    return (y)
-unigram = remove_stopwords(unigram)
-bigram = remove_stopwords(bigram)
-trigram = remove_stopwords(trigram)
- 
-# generate frequency of n-grams
-freq_bi = FreqDist(bigram)
-freq_tri = FreqDist(trigram)
- 
-d = defaultdict(Counter)
-for a, b, c in freq_tri:
-    if(a != None and b!= None and c!= None):
-      d[a, b] += freq_tri[a, b, c]
-       
- 
-# Next word prediction     
-s=''
-def pick_word(counter):
-    "Chooses a random element."
-    return random.choice(list(counter.elements()))
-prefix = "he", "said"
-print(" ".join(prefix))
-s = " ".join(prefix)
-for i in range(19):
-    suffix = pick_word(d[prefix])
-    s=s+' '+suffix
-    print(s)
-    prefix = prefix[1], suffix
+from os import path
+from nltk import ngrams
+
+
+class FiveGram:
+
+    def __init__(self, data_address):
+        self.train = open(path.join(data_address, "train.txt"), mode="r", encoding="utf-8").readlines()
+        self.train = [line.strip().lstrip().rstrip() for line in self.train]
+        self.test = open(path.join(data_address, "test.txt"))
+        self.freqs = {}
+        self.paddings_start = ["s1", "s2", "s3", "s4"]
+        self.paddings_end = ["e1", "e2", "e3", "e4"]
+        self.vocabs = []
+        for line in self.train:
+            line = line.split(" ")
+            for tkn in line:
+                if tkn not in self.vocabs:
+                    self.vocabs.append(tkn)
+        self.vocabs += self.paddings_start + self.paddings_end
+        
+    def create_ngrams_tokens(self):
+        # self.fivegrams = []
+        for line in self.train:
+            line = line.split(" ")
+            line[0:0] = self.paddings_start
+            line[len(line):len(line)] = self.paddings_end
+            fourgrams = ngrams(line, 4)
+            fivegrams = ngrams(line, 5)
+            for fivegram in fivegrams:
+                self.freqs[' '.join(fivegram)] = self.freqs.get(fivegram, 0) + 1
+            for fourgram in fourgrams:
+                self.freqs[' '.join(fourgram)] = self.freqs.get(fourgram, 0) + 1
+
+    def predict(self, text):
+        text = text.split(" ")
+        text[0:0] = self.paddings_start
+        probs = {}
+        for token in self.vocabs:
+            probs[' '.join(text+[token])] = float(
+                (1+self.freqs.get(' '.join(text[-4:])+" "+token, 0)) / (len(self.vocabs)+self.freqs[' '.join(text[-4:])])
+            )
+        return max(probs, key=probs.get).split(" ")[-1]
+
+
+text = "i am moein salimi"
+fivegram = FiveGram("data")
+fivegram.create_ngrams_tokens()
+x = fivegram.predict(text)
+print(x)
